8ab7fc684bbcd3b7d0970e317f9bc25940104890
Ratmir Gaitov <gaitovrat@gmail.com>
2021-02-28 18:26:04 +0100

    Add data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,6 +1,14 @@
 export default {
     hirogana: [
-        { text: 'あ', include: true },
-        { text: 'い', include: true }
+        { textJapanese: 'あ', textRomandzi: 'a', include: true },
+        { textJapanese: 'い', textRomandzi: 'i', include: true },
+        { textJapanese: 'う', textRomandzi: 'u', include: true },
+        { textJapanese: 'え', textRomandzi: 'e', include: true },
+        { textJapanese: 'お', textRomandzi: 'o', include: true },
+        { textJapanese: 'か', textRomandzi: 'ka', include: true },
+        { textJapanese: 'き', textRomandzi: 'ki', include: true },
+        { textJapanese: 'く', textRomandzi: 'ku', include: true },
+        { textJapanese: 'け', textRomandzi: 'ke', include: true },
+        { textJapanese: 'こ', textRomandzi: 'ko', include: true },
     ]
 }
